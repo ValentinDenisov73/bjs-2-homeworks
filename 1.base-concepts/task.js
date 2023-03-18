a977f5037b1b81d7fc86c4eaa239c4ac9683ffcf
Valentin Denisov <valentindenisov1989@gmail.com>
2023-03-18 15:23:47 +0400

    Задача 1 и задача 2 готовы

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,37 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    if(a == 0) {
+      return false;
+    }
+    let result = [];
+    let d = Math.pow(b, 2) - 4 * a * c;
+    console.log("Дискриминант равен: " + d);
+    if (d < 0) {
+      console.log("Корней нет");
+    }
+    if (d == 0) {
+      console.log("Один корень");
+      result.push(-b / (2 * a));
+    }
+    if (d > 0) {
+      console.log("Два корня");
+      result.push((-b + Math.sqrt(d)) / (2 * a));
+      result.push((-b - Math.sqrt(d)) / (2 * a));
+    }
+    return result;
 }
+console.log(solveEquation(1,1,-56));
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  }
+ let interestRate = (percent / 100) / 12;
+ let residualAmount = amount - contribution;
+ let monthlyPayment = residualAmount * (interestRate + (interestRate / (((1 + interestRate)**countMonths) - 1)));
+ let sum = +(monthlyPayment * countMonths).toFixed(2);
+ return sum;
 }
+console.log("Сумма выплаты: " + calculateTotalMortgage(10,0,50000,12));
