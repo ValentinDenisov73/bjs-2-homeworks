84e914986a92042ba383ce2a9dc5522b2d528652
Valentin Denisov <valentindenisov1989@gmail.com>
2023-03-30 15:28:27 +0400

    Задачи 1, 2 готовы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
+	return (arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]))
 }
 
+
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age, index, arr) => acc + age / arr.length, 0);
+	return result;
 }
